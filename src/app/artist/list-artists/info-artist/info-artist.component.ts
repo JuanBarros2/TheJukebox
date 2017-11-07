2b2226e4b7264d705978126d762c71a5f08e1348
Juan Barros <juanlyrabarros@gmail.com>
2017-11-07 15:00:10 -0300

    Listando músicas do artista

--- src/app/artist/list-artists/info-artist/info-artist.component.ts
+++ src/app/artist/list-artists/info-artist/info-artist.component.ts
@@ -1,3 +1,4 @@
+import { Music } from './../../../music/music';
 import { MusicService } from '../../../music/music.service';
 import { Album } from './../../../album/album';
 import { Artist } from '../../artist';
@@ -12,6 +13,7 @@ export class InfoArtistComponent implements OnInit {
 
   @Input() artist: Artist;
   albums: Album[];
+  musics: Music[];
 
   constructor(private musicService: MusicService) {
 
@@ -19,14 +21,28 @@ export class InfoArtistComponent implements OnInit {
 
   ngOnInit() {
     this.albums = this.getAlbuns(this.artist);
+    this.musics = this.getMusics(this.albums, this.artist);
   }
 
   favoriteArtist(artist: Artist) {
     artist.favorite = !artist.favorite;
   }
 
-  getAlbuns(artist: Artist): Album[] {
+  private getAlbuns(artist: Artist): Album[] {
     return this.musicService.getAlbuns(artist);
+  }
+
+  private getMusics(albums: Album[], artist: Artist): Music[] {
+    const resultMusics = [];
+    for (const album of albums) {
+      for (const indexMusic of Object.keys(album.musics)) {
+        const music = album.musics[indexMusic];
+        if (music.artist.toUpperCase() === artist.name.toUpperCase()) {
+          resultMusics.push(music);
+        }
+      }
+    }
+    return resultMusics;
   }
 
 }
